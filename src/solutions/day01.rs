c9161e427663adf214f169e84c05e5e8c2a089b8
avandesa <alex@avandesa.dev>
2022-12-01 20:07:42 -0500

    [DAY 01] Solve day 1 puzzles

--- src/solutions/day01.rs
+++ src/solutions/day01.rs
@@ -1,26 +1,40 @@
 use crate::PuzzleSolver;
 
 pub struct Day01 {
-    _lines: Vec<String>,
+    bags: Vec<Bag>,
 }
 
 impl PuzzleSolver for Day01 {
     fn with_input(input: String) -> Self {
-        Self {
-            _lines: input.lines().map(|l| l.to_string()).collect(),
-        }
+        let mut bags: Vec<Bag> = input.split("\n\n").map(Bag::from_str).collect();
+        bags.sort_by(|a, b| b.0.cmp(&a.0));
+        Self { bags }
     }
 
     fn part1(&self) -> String {
-        // Implement a solution to part 1 here
-        0.to_string()
+        self.bags[0].0.to_string()
     }
 
     fn part2(&self) -> String {
-        // Implement a solution to part 2 here
-        0.to_string()
+        self.bags
+            .iter()
+            .take(3)
+            .map(|b| b.0)
+            .sum::<u32>()
+            .to_string()
+    }
+}
+
+#[derive(Debug)]
+struct Bag(u32);
+
+impl Bag {
+    fn from_str(input: &str) -> Self {
+        let items: Vec<u32> = input.lines().map(|n| n.parse().unwrap()).collect();
+        let total = items.iter().sum();
+        Self(total)
     }
 }
 
 #[cfg(test)]
-crate::test_helpers::test_short_input_for_day!(1, "0", "0");
+crate::test_helpers::test_short_input_for_day!(1, "24000", "45000");
